0dbbe7f397d167cc53cb34e7e9d5893994c87311
Stefan Hagen <stefan@hagen.link>
2021-03-14 23:02:44 +0100

    Mock some cli capablity

--- fuzzy/cli.py
+++ fuzzy/cli.py
@@ -1,1 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long
+"""Just some fuzzy application asking for tests."""
+import os
+import sys
 
+import fuzzy.fuzzy as fz
+
+
+# pylint: disable=expression-not-assigned
+def main(argv=None):
+    """Process the job."""
+    argv = sys.argv[1:] if argv is None else argv
+    fz.main(argv)
