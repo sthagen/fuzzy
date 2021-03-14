daf45f61d3377dc48ee525b4d679b607e081b7c7
Stefan Hagen <stefan@hagen.link>
2021-03-14 23:03:18 +0100

    Mock some application code

--- fuzzy/fuzzy.py
+++ fuzzy/fuzzy.py
@@ -1,1 +1,19 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=expression-not-assigned,line-too-long
+"""Whatever this app does, is more for enabling tests."""
+import os
+import pathlib
+import pymc3
+import subprocess
+import sys
 
+DEBUG_VAR = "FZ_DEBUG"
+DEBUG = os.getenv(DEBUG_VAR)
+
+
+def main(argv=None):
+    """Drive the validator."""
+    argv = argv if argv else sys.argv[1:]
+    if not argv:
+        print("ERROR arguments expected.", file=sys.stderr)
+        return 2
